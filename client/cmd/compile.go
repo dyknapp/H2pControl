49a435c6eb9d247a87a08fe5e44b7ac7bc8bf594
Tijmen H. <41590471+Tzaimun@users.noreply.github.com>
2025-05-01 16:13:06 +0200

    Update compile.go

--- client/cmd/compile.go
+++ client/cmd/compile.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -15,30 +16,75 @@ import (
 	"h2pcontrol.client/internal"
 )
 
-const colorPurple = "\033[35m"
-const colorNone = "\033[0m"
+const (
+	colorPurple = "\033[37m"
+	colorNone   = "\033[0m"
 
-// todo: make into nice functions :)
+	pythonBetterprotoPackage = "betterproto"
+	pythonBuildModule        = "build"
+	pythonPipCommand         = "pip"
+	pyprojectTomlFile        = "pyproject.toml"
 
-// should:
-// build
-// put in site-packages
-// error handling
-// nice printing
+	tempDirPrefix = "go-h2pcontrol"
+	tempLibSuffix = "-h2pcontrol-lib"
+	tempBuildDir  = "h2pcontrol-temp"
+	srcDirName    = "src"
+	distDirName   = "dist"
+
+	wheelExt = ".whl"
+	gzipExt  = ".gz"
+
+	buildNoIsolationFlag = "--no-isolation"
+	pipForceReinstall    = "--force-reinstall"
+
+	protocPythonOutFlag = "--python_betterproto2_out="
+	protocIncludeFlag   = "-I"
+	protoFilePattern    = "*.proto"
+)
+
+var progressMessages = [...]string{
+	"[1/4] Compiling proto files...",
+	"[2/4] Copying pyproject.toml...",
+	"[3/4] Building package...",
+	"[4/4] Installing package locally...",
+}
+
+const (
+	errProtoDirRequired = "Error: --proto-dir is required"
+	errNameRequired     = "Error: --name is required"
+	errNoProtoFiles     = "no .proto files found in %s"
+	errProtocFailed     = "failed to run protoc command: %v"
+	errPythonBuild      = "python build failed: %v"
+	errPipInstall       = "pip install failed: %v"
+	errNoDistFile       = "no .tar.gz or .whl file found in dist directory"
+	errExtractProtoc    = "Failed to extract protoc: %v"
+)
+
+var (
+	protoDir string
+	name     string
+)
 
 var compile = &cobra.Command{
 	Use:   "compile",
 	Short: "Compile proto files",
 	Long:  "Compile your proto files and put them in your site-packages",
-	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		protoDir := filepath.Join(args[0])
+		if protoDir == "" {
+			fmt.Fprintf(os.Stderr, "Error: --proto-dir is required\n")
+			os.Exit(1)
+		}
+		if name == "" {
+			fmt.Fprintf(os.Stderr, "Error: --name is required\n")
+			os.Exit(1)
+		}
 
 		ep, err := python.NewEmbeddedPython("betterproto")
 		check(err)
 
 		tmpDir := filepath.Join(os.TempDir(), "go-h2pcontrol")
-		// Get the python libraries
+
+		// Get the python libraries we embedded
 		ef, err := embed_util.NewEmbeddedFilesWithTmpDir(data.Data, tmpDir+"-h2pcontrol-lib", true)
 		check(err)
 
@@ -51,29 +97,27 @@ var compile = &cobra.Command{
 			os.Exit(1)
 		}
 
-		// TEMP THIS SHUOLD GO TO ACTUAL TMP
-		outDir := filepath.Join("test", "tmp")
+		outDir := filepath.Join(os.TempDir(), "h2pcontrol-temp")
 		os.MkdirAll(outDir, 0755)
 
 		// Compile proto files
-		fmt.Printf("%s[1/4] Compiling proto files...\n%s", colorPurple, colorNone)
-
-		// TEMP
-		os.MkdirAll(outDir+"/src", 0755)
-		err = protoCompile(protocPath, protoDir, outDir+"/src")
+		fmt.Printf("%s%s\n%s", colorPurple, progressMessages[0], colorNone)
+		protoOutDir := outDir + fmt.Sprintf("/src/%s/", name)
+		os.MkdirAll(protoOutDir, 0755)
+		err = protoCompile(protocPath, protoDir, protoOutDir)
 		check(err)
 
 		// Move the pyproject.toml file, required to make a build
-		fmt.Printf("%s[2/4] Copying pyproject.toml...\n%s", colorPurple, colorNone)
+		fmt.Printf("%s%s\n%s", colorPurple, progressMessages[1], colorNone)
 		copyPyrojectToml(outDir)
 
 		// Build the package
-		fmt.Printf("%s[3/4] Building package...\n%s", colorPurple, colorNone)
+		fmt.Printf("%s%s\n%s", colorPurple, progressMessages[2], colorNone)
 		err = buildPackage(ep, outDir)
 		check(err)
 
 		// Install the dist locally
-		fmt.Printf("%s[4/4] Installing package locally...\n%s", colorPurple, colorNone)
+		fmt.Printf("%s%s\n%s", colorPurple, progressMessages[3], colorNone)
 
 		distFile, err := findBuildPackage(outDir)
 		check(err)
@@ -81,27 +125,24 @@ var compile = &cobra.Command{
 
 		fmt.Printf("%sPackage installed into your Python environment's site-packages.%s\n", colorPurple, colorNone)
 
-		// // Clean up
-		// err = os.RemoveAll(outDir)
-		// check(err)
-
-		// err = os.Remove(outDir)
-		// check(err)
+		// Clean up
+		err = os.RemoveAll(outDir)
+		check(err)
 	},
 }
 
 func installPackage(distFile string) {
-	pipCmd := exec.Command("pip", "install", "--force-reinstall", distFile)
+	pipCmd := exec.Command(pythonPipCommand, "install", pipForceReinstall, distFile)
 	pipCmd.Stdout = os.Stdout
 	pipCmd.Stderr = os.Stderr
 	if err := pipCmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "pip install failed: %v\n", err)
+		fmt.Fprintf(os.Stderr, errPipInstall+"\n", err)
 		os.Exit(1)
 	}
 }
 
 func findBuildPackage(outDir string) (string, error) {
-	distDir := filepath.Join(outDir, "dist")
+	distDir := filepath.Join(outDir, distDirName)
 	files, err := os.ReadDir(distDir)
 	if err != nil {
 		panic(err)
@@ -109,29 +150,29 @@ func findBuildPackage(outDir string) (string, error) {
 
 	var distFile string
 	for _, f := range files {
-		if !f.IsDir() && (filepath.Ext(f.Name()) == ".gz" || filepath.Ext(f.Name()) == ".whl") {
+		if !f.IsDir() && (filepath.Ext(f.Name()) == gzipExt || filepath.Ext(f.Name()) == wheelExt) {
 			distFile = filepath.Join(distDir, f.Name())
 			break
 		}
 	}
 	if distFile == "" {
-		return "", fmt.Errorf("no .tar.gz or .whl file found in dist directory")
+		return "", errors.New(errNoDistFile)
 	}
 	return distFile, nil
 }
 
 func copyPyrojectToml(outDir string) {
-	srcFile, err := os.Open("pyproject.toml")
+	srcFile, err := os.Open(pyprojectTomlFile)
 	check(err)
 	content, err := io.ReadAll(srcFile)
 	check(err)
-	fmt.Printf("Contents of %s:\n%s\n", "pyproject.toml", string(content))
+	fmt.Printf("Contents of %s:\n%s\n", pyprojectTomlFile, string(content))
 
 	// Rewind the file pointer to the beginning for copying
 	_, err = srcFile.Seek(0, io.SeekStart)
 	check(err)
 	defer srcFile.Close()
-	destFile, err := os.Create(filepath.Join(outDir, "pyproject.toml"))
+	destFile, err := os.Create(filepath.Join(outDir, pyprojectTomlFile))
 	check(err)
 	defer destFile.Close()
 
@@ -144,14 +185,14 @@ func copyPyrojectToml(outDir string) {
 
 func protoCompile(protocPath string, protoDir string, outDir string) error {
 
-	protoFiles, err := filepath.Glob(filepath.Join(protoDir, "*.proto"))
+	protoFiles, err := filepath.Glob(filepath.Join(protoDir, protoFilePattern))
 	if err != nil || len(protoFiles) == 0 {
-		return fmt.Errorf("no .proto files found in %s", protoDir)
+		return fmt.Errorf(errNoProtoFiles, protoDir)
 	}
 
 	protocArgs := []string{
-		"--python_betterproto2_out=" + outDir,
-		"-I", protoDir,
+		protocPythonOutFlag + outDir,
+		protocIncludeFlag, protoDir,
 	}
 	protocArgs = append(protocArgs, protoFiles...)
 	protocCmd := exec.Command(protocPath, protocArgs...)
@@ -185,6 +226,12 @@ func check(err error) {
 }
 
 func init() {
-	compile.Flags()
+	compile.Flags().StringVar(&protoDir, "proto-dir", "", "Directory containing proto files (required)")
+	compile.Flags().StringVar(&name, "name", "", "Name of the package (required)")
+
+	// Mark flags as required
+	compile.MarkFlagRequired("proto-dir")
+	compile.MarkFlagRequired("name")
+
 	rootCmd.AddCommand(compile)
 }
