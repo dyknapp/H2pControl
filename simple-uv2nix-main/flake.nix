ad09bc253a653c19731c1d6101c505f5130b97d5
laserlab <Tijmen Hilgenkamp>
2025-05-12 14:39:29 +0200

    fix flake to compile

--- simple-uv2nix-main/flake.nix
+++ simple-uv2nix-main/flake.nix
@@ -41,17 +41,37 @@
         pythonparser = prev.pythonparser.overrideAttrs (old: {
           buildInputs = (old.buildInputs or [ ]) ++ [ final.setuptools ];
         });
-        pyqt6 = prev.pyqt6.overrideAttrs (old: {
-          buildInputs = (old.buildInputs or [ ]) ++ [
-            pkgs.qt6.full
-            pkgs.qt6.qtbase
-            pkgs.qt6.qtsvg
-            pkgs.qt6.qttools
-          ];
-          # nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
-          #   pkgs.qt6.wrapQtApps
-          # ];
-        });
+	pyqt6 = prev.pyqt6.overrideAttrs (old: {
+	  dontWrapQtApps = true;
+
+	  #nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ 
+	  nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ 
+	    pkgs.autoPatchelfHook
+	    #pkgs.qt6.wrapQtAppsHook
+	  ];
+	  propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ (with pkgs; [
+            qt6.qtbase
+            qt6.qtsvg
+            qt6.qtdeclarative
+            qt6.qtsensors
+	    qt6.qttools
+	    qt6.qtmultimedia
+	    kdePackages.qtspeech
+	    kdePackages.qtserialport
+	    kdePackages.qtwebchannel
+	    kdePackages.qtscxml
+	    kdePackages.qtwebengine
+	    kdePackages.qtconnectivity
+	    kdePackages.qtremoteobjects
+	    kdePackages.full
+	   # qt6.full
+	    #qt6.webchannel
+	   # qt6.webchannel
+    	  ]);
+  	});
+ # pyqt6 = prev.pyqt6.overrideAttrs (old: {
+  #      buildInputs = (old.buildInputs or [ ]) ++ [ final.setuptools ];
+   #     });
       };
 
       pkgs = nixpkgs.legacyPackages.${system};
@@ -72,28 +92,6 @@
       packages.${system}.default = pythonSet.mkVirtualEnv "artiq-env" workspace.deps.default;
 
       devShells.${system} = {
-        # Impure development shell
-        impure = pkgs.mkShell {
-          packages = [
-            python
-            pkgs.uv
-            pkgs.qt6.full
-          ];
-          env = {
-            UV_PYTHON_DOWNLOADS = "never";
-            UV_PYTHON = python.interpreter;
-          } // lib.optionalAttrs pkgs.stdenv.isLinux {
-            LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
-              pythonManylinuxPackages.manylinux1
-              qt6.full
-              qt6.qtbase
-              qt6.qtsvg
-            ]);
-          shellHook = ''
-            unset PYTHONPATH
-          '';
-        };
-
         # Pure development shell using uv2nix
         default = 
           let
@@ -108,21 +106,33 @@
             packages = [
               virtualenv
               pkgs.uv
-              pkgs.qt6.full 
             ];
 
             env = {
               UV_NO_SYNC = "1";
-              UV_PYTHON = "${virtualenv}/bin/python";
+              UV_PYTHON = python.interpreter;
               UV_PYTHON_DOWNLOADS = "never";
-
-              QT_PLUGIN_PATH = "${pkgs.qt6.full}/lib/qt-6/plugins";
+    	   } // lib.optionalAttrs pkgs.stdenv.isLinux {
+#	      LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+#                pythonManylinuxPackages.manylinux1
+#              ]);
+	      #1
+	      #LD_LIBRARY_PATH = "${pkgs.libGL}/lib";
             };
 
             shellHook = ''
               unset PYTHONPATH
               export REPO_ROOT=$(git rev-parse --show-toplevel)
-              
+
+              echo "========================================="
+              echo "Debug information:"
+              echo "Current directory: $(pwd)"
+              #echo "REPO_ROOT: $REPO_ROOT"
+              #echo "PYTHONPATH: $PYTHONPATH"
+              #echo "PATH: $PATH"
+              echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+              echo "========================================="
+
               echo "----------------------------------------"
               echo "🚀 ARTIQ development environment activated"
               echo "Python: $(python --version)"
@@ -133,6 +143,5 @@
             '';
           };
       };
-    };
   };
 }
